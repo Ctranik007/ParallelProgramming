2acb4242a87ad7caf0d1a5b8f2854c20677638fa
Dyusekenov Dzhalil <dyusekenov.dzhalil@gmail.com>
2021-09-18 14:32:32 +0600

    add avalible to interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Pattern pattern = Pattern.compile("(class|interface)(\\s+)(\\w+)(<.*>)?(\\s+extends\\s+)(\\w+)");
+        long m = System.currentTimeMillis();
+        Pattern pattern = Pattern.compile("(class|interface)(\\s+)(\\w+)(<.*>)?(\\s+extends(\\s+\\w+(,\\s+\\w+)*))((\\s+implements)(\\s+\\w+(,\\s+\\w+)*))*");
 
-        Map<String,Set<String>> invertedIndexMap = new HashMap<>();
-        Files.find(Paths.get("C:\\Users\\pavel\\Downloads\\spring-framework-main\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
-                && p.getFileName().toString().matches(".*\\.java")).forEach(x->{
+        Map<String, Set<String>> invertedIndexMap = new HashMap<>();
+        Files.find(Paths.get("G:\\study\\7 семестр\\параллельное\\лб2\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
+                && p.getFileName().toString().matches(".*\\.java")).forEach(x -> {
                     try {
                         Files.lines(x).forEach(
-                                y->{
+                                y -> {
                                     Matcher matcher = pattern.matcher(y);
                                     if (matcher.find()) {
-                                        Set<String> set = invertedIndexMap.getOrDefault(matcher.group(6),new HashSet<>());
-                                        set.add(matcher.group(1) + " " +matcher.group(3));
-                                        invertedIndexMap.put(matcher.group(6),set);
+                                        Arrays.stream(matcher.group(6).split(",")).forEach(k -> {
+                                            Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
+                                            set.add(matcher.group(1) + " " + matcher.group(3));
+                                            invertedIndexMap.put(k, set);
+                                        });
+                                        if (matcher.group(10) != null) {
+                                            Arrays.stream(matcher.group(10).split(",")).forEach(k -> {
+                                                Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
+                                                set.add(matcher.group(1) + " " + matcher.group(3));
+                                                invertedIndexMap.put(k, set);
+                                            });
+                                        }
+
                                     }
 
                                 }
@@ -33,8 +41,9 @@ public class Main {
                 }
         );
 
-        invertedIndexMap.keySet().forEach(k-> System.out.println("Класс " + k + " базовый для " + invertedIndexMap.get(k)));
-        System.out.println(invertedIndexMap.size());
+        invertedIndexMap.keySet().forEach(k -> System.out.println(k + " базовый для " + invertedIndexMap.get(k)));
+        System.out.println("Количество классов и интерфейсов : " + invertedIndexMap.size());
+        System.out.println("Время выполнения мс : " + (double) (System.currentTimeMillis() - m));
     }
 }
 // (((|public|final|abstract|private|static|protected)(\s+))?(class)(\s+)(\w+)(<.*>)?(\s+extends\s+)(\w+)?(<.*>)?(\s+implements\s+)?(.*)?(<.*>)?(\w*))
