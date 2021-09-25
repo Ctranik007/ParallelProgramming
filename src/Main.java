76b40301774e820b371f3cec6036eb9ac929c0fd
DZHALIL <DZHALIL@DESKTOP-56BPK09>
2021-09-25 13:57:46 +0600

    add threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
@@ -11,37 +12,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Instant start = Instant.now();
         Pattern pattern = Pattern.compile("(class|interface)(\\s+)(\\w+)(<.*>)?(\\s+extends(\\s+\\w+(,\\s+\\w+)*))((\\s+implements)(\\s+\\w+(,\\s+\\w+)*))*");
-
         Map<String, Set<String>> invertedIndexMap = new HashMap<>();
-        Files.find(Paths.get("C:\\Users\\pavel\\Downloads\\spring-framework-main\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
+
+
+      Files.find(Paths.get("C:\\Users\\pavel\\Downloads\\spring-framework-main\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
                 && p.getFileName().toString().matches(".*\\.java")).forEach(x -> {
-                    try {
-                        Files.lines(x).forEach(
-                                y -> {
-                                    Matcher matcher = pattern.matcher(y);
-                                    if (matcher.find()) {
-                                        Arrays.stream(matcher.group(6).split(",")).forEach(k -> {
-                                            Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
-                                            set.add(matcher.group(1) + " " + matcher.group(3));
-                                            invertedIndexMap.put(k, set);
-                                        });
-                                        if (matcher.group(10) != null) {
-                                            Arrays.stream(matcher.group(10).split(",")).forEach(k -> {
-                                                Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
-                                                set.add(matcher.group(1) + " " + matcher.group(3));
-                                                invertedIndexMap.put(k, set);
-                                            });
-                                        }
-
-                                    }
-
+                    Thread thread = new Thread(() -> {
+                        try {
+                            Files.lines(x).map(pattern::matcher).filter(Matcher::find).forEach(matcher -> {
+                                Arrays.stream(matcher.group(6).split(",")).forEach(k -> {
+                                    Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
+                                    set.add(matcher.group(1) + " " + matcher.group(3));
+                                    invertedIndexMap.put(k, set);
+                                });
+                                if (matcher.group(10) != null) {
+                                    Arrays.stream(matcher.group(10).split(",")).forEach(k -> {
+                                        Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
+                                        set.add(matcher.group(1) + " " + matcher.group(3));
+                                        invertedIndexMap.put(k, set);
+                                    });
                                 }
-                        );
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    thread.start();
                 }
         );
+
 
         invertedIndexMap.keySet().forEach(k -> System.out.println(k + " базовый для " + invertedIndexMap.get(k)));
         Instant stop = Instant.now();
