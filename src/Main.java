e1e4db94796153f0ca2987ad8a61a4a1571684e0
DZHALIL <DZHALIL@DESKTOP-56BPK09>
2021-09-25 12:11:27 +0600

    formating time string output

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        long m = System.currentTimeMillis();
+        Instant start = Instant.now();
         Pattern pattern = Pattern.compile("(class|interface)(\\s+)(\\w+)(<.*>)?(\\s+extends(\\s+\\w+(,\\s+\\w+)*))((\\s+implements)(\\s+\\w+(,\\s+\\w+)*))*");
 
         Map<String, Set<String>> invertedIndexMap = new HashMap<>();
-        Files.find(Paths.get("G:\\study\\7 семестр\\параллельное\\лб2\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
+        Files.find(Paths.get("C:\\Users\\pavel\\Downloads\\spring-framework-main\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
                 && p.getFileName().toString().matches(".*\\.java")).forEach(x -> {
                     try {
                         Files.lines(x).forEach(
@@ -42,8 +44,12 @@ public class Main {
         );
 
         invertedIndexMap.keySet().forEach(k -> System.out.println(k + " базовый для " + invertedIndexMap.get(k)));
+        Instant stop = Instant.now();
+
         System.out.println("Количество классов и интерфейсов : " + invertedIndexMap.size());
-        System.out.println("Время выполнения мс : " + (double) (System.currentTimeMillis() - m));
+        Duration duration = Duration.between(start, stop);
+        System.out.println(duration.toMillis());
+        System.out.println(String.format("%d H, %d M, %d S", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart()));
     }
 }
 // (((|public|final|abstract|private|static|protected)(\s+))?(class)(\s+)(\w+)(<.*>)?(\s+extends\s+)(\w+)?(<.*>)?(\s+implements\s+)?(.*)?(<.*>)?(\w*))
