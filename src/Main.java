813f74a23e135a87f0108fbe8c6bf80139c684a4
DZHALIL <ctranik007@gmail.com>
2021-11-25 20:56:30 +0600

    task 6

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,77 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) throws IOException, InterruptedException {
         Instant start = Instant.now();
 
-        List<Thread> threadList = new ArrayList<>();
+        //очередь данных (100 размер), доступ к очереди для потоков, заблокированных при вставке или удалении, обрабатывается в порядке FIFO
+        BlockingQueue<String> tasksQueue = new ArrayBlockingQueue<>(100, true);
 
-        Map<String, Set<String>> invertedIndexMap = new HashMap<>();
-        Files.find(Paths.get("D:\\7 семестр\\параллельное\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
-                && p.getFileName().toString().matches(".*\\.java")).forEach(x -> {
+        //очередь для результатов потоков
+        BlockingQueue<Map<String, Set<String>>> poolMap = new LinkedBlockingQueue<>();
 
-            RegexThread thread = new RegexThread(x, invertedIndexMap);
-
-            thread.start();
-            threadList.add(thread);
-        });
-
-        for (Thread thread : threadList) {
-            thread.join();
+        //запускам 6 потоков
+        for (int i = 0; i < 6; i++) {
+            new Thread(new RegexProcessor(tasksQueue, poolMap)).start();
         }
+
+        //раздаем задания для потоков
+        Map(tasksQueue);
+
+        //завершаем потоки с помощью poison pill
+        for (int i = 0; i < 6; i++) {
+            tasksQueue.put("stop");
+        }
+
+        //собираем информацию полученную от потоков в одну Map
+        Map<String, Set<String>> invertedIndexMap  = Reducer(poolMap);
         Instant stop = Instant.now();
-        invertedIndexMap.keySet().forEach(k -> System.out.println(k + " базовый для " + invertedIndexMap.get(k)));
-        System.out.println("Количество классов и интерфейсов : " + invertedIndexMap.size());
+
+        invertedIndexMap .forEach((x, y) -> System.out.println(x + " базовый для " + y));
+
+        System.out.println("Количество классов и интерфейсов : " + invertedIndexMap .size());
 
         Duration duration = Duration.between(start, stop);
         System.out.println(duration.toMillis());
         System.out.printf("%d H, %d M, %d S%n", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
     }
+
+    //Метод для объединения результатов потоков
+    private static Map<String, Set<String>> Reducer(BlockingQueue<Map<String, Set<String>>> maps) throws InterruptedException {
+        Map<String, Set<String>> map = new HashMap<>();
+        while (!maps.isEmpty()) {
+            map = Stream.concat(map.entrySet().stream(), maps.take().entrySet().stream())
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> Stream.concat(e1.stream(), e2.stream())
+                                    .collect(Collectors.toSet())
+                    ));
+        }
+        return map;
+    }
+
+    //Метод применяющий нужную функцию к каждому элементу списка tasks
+    private static void Map(BlockingQueue<String> tasks) throws IOException {
+        Files.find(Paths.get("G:\\учёба университет\\7 семестр\\параллельное\\spring-framework-main"), 999, (p, bfa) -> bfa.isRegularFile()
+                && p.getFileName().toString().matches(".*\\.java")).forEach(x -> {
+            try {
+                tasks.put(x.toString());
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
 }
+
---
+++ src/RegexProcessor.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexProcessor implements Runnable {
+    private final BlockingQueue<Map<String, Set<String>>> poolMap;
+    private final BlockingQueue<String> tasksQueue;
+    private final String poison = "stop";
+
+    public RegexProcessor(BlockingQueue<String> tasksQueue, BlockingQueue<Map<String, Set<String>>> poolMap) {
+        this.tasksQueue = tasksQueue;
+        this.poolMap = poolMap;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String file;
+            while (!((file = tasksQueue.take()).equals(poison))) {
+                //в каждом потоке создаем свою HashMap
+                Map<String, Set<String>> invertedIndexMap = new HashMap<>();
+                Files.lines(Path.of(file)).forEach(
+                        y -> {
+                            Pattern pattern = Pattern.compile("(class|interface)(\\s+)(\\w+)(<.*>)?(\\s+extends(\\s+\\w+(,\\s+\\w+)*))((\\s+implements)(\\s+\\w+(,\\s+\\w+)*))*");
+                            Matcher matcher = pattern.matcher(y);
+                            if (matcher.find()) {
+                                for (String s : matcher.group(6).split(",")) {
+                                    Set<String> set = invertedIndexMap.getOrDefault(s, new HashSet<>());
+                                    set.add(matcher.group(1) + " " + matcher.group(3));
+                                    invertedIndexMap.put(s, set);
+
+                                }
+                                if (matcher.group(10) != null) {
+                                    Arrays.stream(matcher.group(10).split(",")).forEach(k -> {
+                                        Set<String> set = invertedIndexMap.getOrDefault(k, new HashSet<>());
+                                        set.add(matcher.group(1) + " " + matcher.group(3));
+                                        invertedIndexMap.put(k, set);
+                                    });
+                                }
+                            }
+                        });
+                //добавляем HashMap текущего потока в общую очередь
+                poolMap.put(invertedIndexMap);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
